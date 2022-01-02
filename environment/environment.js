1a5bb2d3b99951d0ecf82bd1bb3fd400f2ce7390
Sócrates López <josesocrateslopez@gmail.com>
2022-01-02 16:28:15 -0600

    prueba de deploy en heroku 2

--- environment/environment.js
+++ environment/environment.js
@@ -1,10 +1,10 @@
 const environment = {
-    host: 'localhost',
+    host: 'bzjtpxqdncnzw5mk1nb6-mysql.services.clever-cloud.com',
     port: 3306,
-    user: 'root',
-    password: '',
-    database_name: 'db_graphql',
-    production: false,
+    user: 'uws26fjdloqbmswo',
+    password: 'c7qtEtTDHThUFQuMl0sW',
+    database_name: 'bzjtpxqdncnzw5mk1nb6',
+    production: true,
 }
 module.exports = {
     environment
