feb5e7119b55a848378419fbc3456fcdbc3eb0e9
Sócrates López <josesocrateslopez@gmail.com>
2022-01-01 19:40:13 -0600

    integracion de sequalize, graphql funcional

---
+++ init_db.js
@@ -0,0 +1,78 @@
+
+const { get } = require('express/lib/response');
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize('db_graphql', 'root', '',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+const moment = require('moment')
+const Employee = sequelize.define('Employee',{
+    id_employee: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nationality: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    civil_status: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        get(){
+            return moment(this.getDataValue('birthday')).add(1, 'day').format('DD/MMMM/YYYY')
+        }
+    }
+})
+
+const User = sequelize.define('User', {
+    id_user: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+
+
+function syncdb(){
+    sequelize.sync({ force: false });
+}
+
+
+module.exports = {
+    Employee,
+    User,
+    syncdb
+}
+
